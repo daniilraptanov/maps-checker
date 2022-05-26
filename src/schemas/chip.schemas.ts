b1d30aebad723335ac1e94e0d36093bc135ff071
Daniil Raptanov <daniilraptanov@gmail.com>
2022-05-26 15:28:39 +0300

    server schema changes

--- src/schemas/chip.schemas.ts
+++ src/schemas/chip.schemas.ts
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 export const chipSchema = Joi.object({
-    id: Joi.string(),
+    id: Joi.string().allow(""),
     name: Joi.string().required(),
     level: Joi.number().required(),
     isComplete: Joi.boolean().required(),
