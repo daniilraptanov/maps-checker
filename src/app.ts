d2b982266f406315f3b2e6888bbf37603eadcb46
Daniil Raptanov <daniilraptanov@gmail.com>
2023-01-04 20:33:57 +0200

    added default server port

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,12 @@ app.use("/api/v1", require("./routes/api-v1.routes"));
 
 app.use(cors());
 
-app.listen(config.PORT, () => {
+const defaultPort = 5000;
+
+app.listen(config.PORT || defaultPort, () => {
     try {
-        return console.log(`server is listening on ${config.PORT}`);
+        return console.log(`server is listening on ${config.PORT || defaultPort}`);
     } catch {
-        return console.error(`server error (on ${config.PORT})`);
+        return console.error(`server error (on ${config.PORT || defaultPort})`);
     }
 });
